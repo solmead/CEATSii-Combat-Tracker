c4a14cbd364e1bfd6f4388041295672b67822e87
Chris Powell <powelcr@uc.edu>
2022-10-18 09:15:20 -0400

    Docker setup completed

--- CombatTracker/Db/entrypoint.sh
+++ CombatTracker/Db/entrypoint.sh
@@ -1,10 +1,4 @@
 # Run Microsoft SQl Server and initialization script (at the same time)
-#!/usr/bin/env bash
-#set -m
-#/usr/src/app
-#/run-initialization.sh & /opt/mssql/bin/sqlservr
-chmod +x ./run-initilization.sh
-chmod 755 ./run-initilization.sh
+ls
 
-/usr/src/app/run-initialization.sh & /opt/mssql/bin/sqlservr
-#fg
+./run-initialization.sh & /opt/mssql/bin/sqlservr
